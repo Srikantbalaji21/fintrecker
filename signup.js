9f48933a7d2cab01fbd86d2d32d7ccf6db408be8
Srikantbalaji21 <163219481+Srikantbalaji21@users.noreply.github.com>
2024-08-20 08:31:42 +0530

    Create signup.js
    frontend

---
+++ signup.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSignup = async () => {
+    try {
+      const res = await axios.post('/api/auth/signup', { email, password });
+      localStorage.setItem('token', res.data.token

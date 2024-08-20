25d8c1afa7cc63ce9675bd8d5be6e9487574bde3
Srikantbalaji21 <163219481+Srikantbalaji21@users.noreply.github.com>
2024-08-20 08:27:34 +0530

    Create Goal.js

---
+++ Goal.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const goalSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String, required: true },
+  targetAmount: { type: Number, required: true },
+  currentAmount: { type: Number, default: 0 },
+  deadline: { type: Date, required: true },
+});
+
+export default mongoose.model('Goal', goalSchema);
